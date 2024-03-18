bdf96af918b40075d48ea2d861f622509d3b0fbd
Mohamed-Abd-El-Ghany <mb0672922@gmail.com>
2024-03-18 19:15:27 +0200

    Initial commit

--- lib/presenation/components/custom_emergency_container1.dart
+++ lib/presenation/components/custom_emergency_container1.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../app/app_colors.dart';
 import '../../app/app_texts.dart';
@@ -7,47 +7,51 @@ import '../../app/app_texts.dart';
 class CustomEmergencyContainer1 extends StatelessWidget {
   const CustomEmergencyContainer1({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55.h,
-      padding: EdgeInsets.symmetric(horizontal: 7.w, vertical: 7.h),
-      decoration: BoxDecoration(
-        color: AppColors.latte1,
-        border: Border.all(
-          color: AppColors.black,
-        ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(15.r),
-        ),
-      ),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(6),
-            child: Row(
-              children: [
-                Text(
-                  AppText.call1,
-                  style: TextStyle(
-                    fontSize: 15.w,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  width: 95.w,
-                ),
-                Text(
-                  AppText.call112,
-                  style: TextStyle(
-                    fontSize: 15.w,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+    return InkWell(
+      onTap: () async => await FlutterPhoneDirectCaller.callNumber('112'),
+      child: Container(
+        height: 55.h,
+        padding: EdgeInsets.symmetric(horizontal: 7.w, vertical: 7.h),
+        decoration: BoxDecoration(
+          color: AppColors.latte1,
+          border: Border.all(
+            color: AppColors.black,
           ),
-        ],
+          borderRadius: BorderRadius.all(
+            Radius.circular(15.r),
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Row(
+                children: [
+                  Text(
+                    AppText.call1,
+                    style: TextStyle(
+                      fontSize: 15.w,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 95.w,
+                  ),
+                  Text(
+                    AppText.call112,
+                    style: TextStyle(
+                      fontSize: 15.w,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
