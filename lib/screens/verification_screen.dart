ca259041b64f17d106413609ad675ad2c6034704
Mohamed-Abd-El-Ghany <mb0672922@gmail.com>
2024-05-21 23:04:30 +0300

    11

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -1,13 +1,13 @@
+import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../app/app_colors.dart';
 import '../app/app_images.dart';
 import '../app/app_texts.dart';
-import '../presenation/components/custom_button2.dart';
 import '../presenation/components/custom_elevated_button.dart';
-import '../presenation/components/custom_otp_boxs.dart';
 import 'congrats_screen.dart';
 
 final _firebase = FirebaseAuth.instance;
@@ -16,6 +16,7 @@ class VerificationScreen extends StatefulWidget {
   final String? email;
   final String? password;
   final String? phone;
+
   const VerificationScreen({
     super.key,
     required String this.email,
@@ -28,8 +29,36 @@ class VerificationScreen extends StatefulWidget {
 }
 
 class _VerificationScreenState extends State<VerificationScreen> {
+  void initState() {
+    phoneAuth();
+    startTimer();
+    super.initState;
+  }
+
+  int counter = 120;
+
+  late Timer _timer;
+  String? verifId;
+
+  void startTimer() {
+    setState(() {
+      counter = 120;
+    });
+    _timer =Timer.periodic(const Duration(seconds: 1),(timer){
+      if(!mounted || counter == 0)
+      {
+        timer.cancel();
+      }else {
+        setState((){
+          if (counter > 0){
+            counter--;
+          }
+        });
+      }
+    });
+  }
+
   void _submit2() async {
-    _isLoading = true;
     setState(() {});
     try {
       await signUser(widget.email!, widget.password!);
@@ -43,7 +72,7 @@ class _VerificationScreenState extends State<VerificationScreen> {
         MaterialPageRoute(
           builder: (builder) => const CongratsScreen(),
         ),
-        (route) => false,
+            (route) => false,
       );
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context).clearSnackBars();
@@ -57,20 +86,23 @@ class _VerificationScreenState extends State<VerificationScreen> {
     setState(() {});
   }
 
-  Future<void> signUser(String email , String passward) async {
+  Future<void> signUser(String email, String passward) async {
     final UserCredential userCredential =
-        await _firebase.createUserWithEmailAndPassword(
+    await _firebase.createUserWithEmailAndPassword(
       email: email,
       password: passward,
     );
   }
 
   bool _isLoading = false;
-  bool isvalidOtp = false;
+  bool correct = true;
   TextEditingController txt1 = TextEditingController();
   TextEditingController txt2 = TextEditingController();
   TextEditingController txt3 = TextEditingController();
   TextEditingController txt4 = TextEditingController();
+  TextEditingController txt5 = TextEditingController();
+  TextEditingController txt6 = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -109,29 +141,31 @@ class _VerificationScreenState extends State<VerificationScreen> {
                 Padding(
                   padding: EdgeInsets.only(top: 40.h),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      myInputBox(context, txt1),
-                      myInputBox(context, txt2),
-                      myInputBox(context, txt3),
-                      myInputBox(context, txt4),
+                      myInputBox(context, txt1,correct),
+                      myInputBox(context, txt2,correct),
+                      myInputBox(context, txt3,correct),
+                      myInputBox(context, txt4,correct),
+                      myInputBox(context, txt5,correct),
+                      myInputBox(context, txt6,correct),
                     ],
                   ),
                 ),
                 SizedBox(height: 20.h),
                 Text(
-                  isvalidOtp ? 'Invalid otp!' : '',
+                  correct ? '' : 'Invalid otp!',
                   style: const TextStyle(
                     fontSize: 20,
-                    color: Colors.red,
+                    color: AppColors.red,
                   ),
                 ),
                 Container(
                   margin: EdgeInsets.only(top: 18.h),
-                  child: const Text(
-                    AppText.resendInSec,
+                  child: Text(
+                    '${AppText.resendInSec}$counter sec',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 14, color: AppColors.grey1),
+                    style: const TextStyle(fontSize: 14, color: AppColors.grey1),
                   ),
                 ),
                 Padding(
@@ -139,25 +173,7 @@ class _VerificationScreenState extends State<VerificationScreen> {
                   child: CustomElevatedButton(
                     text: AppText.verify,
                     onPressed: () {
-                      final otp = txt1.text + txt2.text + txt3.text + txt4.text;
-                      if (otp == '1989') {
-                        setState(() {
-                          isvalidOtp = false;
-                        });
-                        _submit2();
-                        // Navigator.pushAndRemoveUntil(
-                        //   context,
-                        //   MaterialPageRoute(
-                        //     builder: (builder) => const CongratsScreen(),
-                        //   ),
-                        //   (route) => false,
-                        // );
-                      }
-                      // else {
-                      //   setState(() {
-                      //     isvalidOtp = true;
-                      //   });
-                      // }
+                      sentCode();
                     },
                   ),
                 ),
@@ -168,20 +184,89 @@ class _VerificationScreenState extends State<VerificationScreen> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    txt1.dispose();
+    txt2.dispose();
+    txt3.dispose();
+    txt4.dispose();
+    txt5.dispose();
+    txt6.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void phoneAuth() async {
+    try {
+      await _firebase.verifyPhoneNumber(
+        phoneNumber: widget.phone!,
+        timeout: const Duration(seconds: 120),
+        verificationCompleted: (PhoneAuthCredential credential) {
+          print('Verification completed: $credential');
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print('Verification failed: ${e.message}');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Verification failed: ${e.message}')),
+          );
+        },
+        codeSent: (String verificationId, int? resendToken) async {
+          print('Code sent to ${widget.phone}');
+          verifId = verificationId;
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {
+          print('Auto retrieval timeout for verificationId: $verificationId');
+          verifId = verificationId;
+        },
+      );
+    } catch (e) {
+      print('An error occurred: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred: $e')),
+      );
+    }
+  }
+
+
+  sentCode() async {
+    try {
+      String smsCode = txt1.text + txt2.text + txt3.text + txt4.text + txt5.text + txt6.text;
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verifId!,
+        smsCode: smsCode,
+      );
+      await _firebase.signInWithCredential(credential) .then((value) {
+        if (value.user != null) {
+          setState(() {
+            correct = true;
+            _isLoading = true;
+            _submit2();
+          });
+        }
+      });
+    } catch (ex) {
+      setState(() {
+        correct = false;
+      });
+    }
+  }
 }
 
-Widget myInputBox(BuildContext context, TextEditingController controller) {
+Widget myInputBox(BuildContext context, TextEditingController controller, bool correct) {
   return Container(
     width: 53.w,
     height: 53.h,
-    margin: EdgeInsets.symmetric(horizontal: 8.w),
     decoration: BoxDecoration(
-      boxShadow: const [
+      border: Border.all(
+        width: 0.75.w,
+      ),
+      boxShadow: [
         BoxShadow(
-          color: AppColors.grey1,
+          color: correct ? AppColors.grey1 : AppColors.red,
           blurRadius: .2, // soften the shadow
           spreadRadius: .2, //extend the shadow
-          offset: Offset(
+          offset: const Offset(
             0.5, // Move to right 5  horizontally
             0.5, // Move to bottom 5 Vertically
           ),
@@ -191,17 +276,21 @@ Widget myInputBox(BuildContext context, TextEditingController controller) {
       borderRadius: BorderRadius.circular(15.r),
     ),
     child: TextField(
+      inputFormatters: [LengthLimitingTextInputFormatter(1)],
       controller: controller,
       maxLength: 1,
       textAlign: TextAlign.center,
       keyboardType: TextInputType.number,
       style: const TextStyle(fontSize: 30),
       decoration: const InputDecoration(
+        border: InputBorder.none,
         counterText: '',
       ),
       onChanged: (value) {
         if (value.length == 1) {
           FocusScope.of(context).nextFocus();
+        } else if (value.isEmpty){
+          FocusScope.of(context).previousFocus();
         }
       },
     ),
